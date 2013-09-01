a241430ea1c0bb58317e7c17055da63bb143a3de
Matt Mueller <mattmuelle@gmail.com>
2013-09-01 21:44:07 +0000

    installer and runner working

--- installers/node/index.js
+++ installers/node/index.js
@@ -5,41 +5,35 @@
 var debug = require('debug')('coderunner:node:installer');
 var args = process.argv.slice(2);
 var port = args[0] || 3000;
+var express = require('express');
+var app = module.exports = express();
 var exec = require('child_process').exec;
-var axon = require('axon');
 var escape = require('shell-escape');
-var home = process.env.HOME;
 
 /**
- * Set up the response socket
+ * Routes
  */
 
-var rep = axon
-  .socket('rep')
-  .format('json')
-  .connect(port);
-
-console.log('listening on port: %s', port);
-
-/**
- * Listen for a dependency
- */
-
-rep.on('message', install); 
-
-/**
- * Install the dependency
- */
-
-function install(dep, reply) {
+app.post('/install/:dep', function(req, res, next) {
   var self = this;
+  var dep = req.params.dep;
   var cmd = escape(['npm', 'install', '-s', dep]);
 
   debug('installing %s', dep);
 
   // install the dependency
-  exec(cmd, { cwd: home }, function(err, stdout, stderr) {
+  exec(cmd, { cwd: '/' }, function(err, stdout, stderr) {
     debug('executed npm, err: %s, stdout: %s, stderr: %s', err, stdout, stderr);
-    reply(err, stdout, stderr);
+    if (err) res.send(500, { error: err });
+    else if (stderr) res.send(500, { stderr: stderr });
+    else res.send(200, { stdout: stdout });
   });
-}
+});
+
+/** 
+ * Bind to a port
+ */
+
+app.listen(port, function() {
+  console.log('listening on port: %s', port);
+});
